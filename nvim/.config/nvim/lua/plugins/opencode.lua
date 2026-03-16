0f5211022601e3db3faee8bc09fd00dde65c1167
Andrew Ellis <andrewellis@pm.me>
2026-03-17 00:23:17 +0100

    Add opencode keybindings

--- nvim/.config/nvim/lua/plugins/opencode.lua
+++ nvim/.config/nvim/lua/plugins/opencode.lua
@@ -9,76 +9,44 @@ return {
     -- Your configuration, if any
   },
   keys = {
-    -- Recommended keymaps
     {
-      "<leader>oA",
+      "<leader>aA",
       function()
         require("opencode").ask()
       end,
       desc = "Ask opencode",
     },
     {
-      "<leader>oa",
-      function()
-        require("opencode").ask("@cursor: ")
-      end,
-      desc = "Ask opencode about this",
-      mode = "n",
-    },
-    {
-      "<leader>oa",
-      function()
-        require("opencode").ask("@selection: ")
-      end,
-      desc = "Ask opencode about selection",
-      mode = "v",
-    },
-    {
-      "<leader>ot",
+      "<leader>ao",
       function()
         require("opencode").toggle()
       end,
-      desc = "Toggle embedded opencode",
+      desc = "Toggle opencode",
     },
     {
-      "<leader>on",
+      "<leader>an",
       function()
         require("opencode").command("session_new")
       end,
-      desc = "New session",
+      desc = "New opencode session",
     },
     {
-      "<leader>oy",
+      "<leader>ay",
       function()
         require("opencode").command("messages_copy")
       end,
       desc = "Copy last message",
     },
     {
-      "<S-C-u>",
-      function()
-        require("opencode").command("messages_half_page_up")
-      end,
-      desc = "Scroll messages up",
-    },
-    {
-      "<S-C-d>",
-      function()
-        require("opencode").command("messages_half_page_down")
-      end,
-      desc = "Scroll messages down",
-    },
-    {
-      "<leader>op",
+      "<leader>ap",
       function()
         require("opencode").select_prompt()
       end,
       desc = "Select prompt",
       mode = { "n", "v" },
     },
-    -- Example: keymap for custom prompt
     {
-      "<leader>oe",
+      "<leader>ax",
       function()
         require("opencode").prompt("Explain @cursor and its context")
       end,
