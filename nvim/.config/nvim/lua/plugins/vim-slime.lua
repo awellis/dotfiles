90c4d32a1413c46acc14a6924d645403ab68fc06
Andrew Ellis <andrewellis@pm.me>
2026-03-20 13:43:21 +0100

    Add vim-tmux-navigator plugin and update vim-slime configuration

--- nvim/.config/nvim/lua/plugins/vim-slime.lua
+++ nvim/.config/nvim/lua/plugins/vim-slime.lua
@@ -2,12 +2,15 @@ return {
   -- slime (REPL integration)
   {
     "jpalardy/vim-slime",
+    ft = { "julia", "python", "r", "rmd", "quarto" },
     keys = {
       { "<leader>rC", "<cmd>SlimeConfig<cr>", desc = "Slime Config" },
       { "<leader>rr", "<Plug>SlimeSendCell<BAR>/^# %%<CR>", desc = "Slime Send Cell" },
       { "<leader>rr", ":<C-u>'<,'>SlimeSend<CR>", mode = "v", desc = "Slime Send Selection" },
+      { "<leader>rl", "<Plug>SlimeLineSend", desc = "Slime Send Line" },
+      { "<leader>rp", "<Plug>SlimeParagraphSend", desc = "Slime Send Paragraph" },
     },
-    config = function()
+    init = function()
       vim.g.slime_target = "tmux"
       vim.g.slime_default_config = { socket_name = "default", target_pane = "{last}" }
       vim.g.slime_cell_delimiter = "# %%"
