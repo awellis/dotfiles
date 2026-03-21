ca8c11a4f3f1c6b0f4efe3a63177f1ab3c3645aa
Andrew Ellis <andrewellis@pm.me>
2026-03-22 00:56:44 +0100

    Add IWE config

---
+++ nvim/.config/nvim/lua/plugins/iwe.lua
@@ -0,0 +1,21 @@
+return {
+  "iwe-org/iwe.nvim",
+  ft = "markdown",
+  config = function()
+    require("iwe").setup({
+      lsp = {
+        auto_format_on_save = true,
+        enable_inlay_hints = true,
+      },
+      mappings = {
+        enable_markdown_mappings = true,
+        enable_picker_keybindings = true,
+        enable_lsp_keybindings = true,
+        enable_preview_keybindings = true,
+      },
+      picker = {
+        backend = "snacks",
+      },
+    })
+  end,
+}
