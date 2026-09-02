a2103179b88b3f182d2221e86e7407f12deb60ad
Andrew Ellis <andrewellis@pm.me>
2026-09-02 22:44:16 +0200

    Update IWE nvim config

--- nvim/.config/nvim/lua/plugins/iwe.lua
+++ nvim/.config/nvim/lua/plugins/iwe.lua
@@ -2,11 +2,34 @@ return {
   "iwe-org/iwe.nvim",
   ft = "markdown",
   config = function()
+    local lsp_group = vim.api.nvim_create_augroup("iwe_lsp", { clear = true })
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = lsp_group,
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if not client or client.name ~= "iwes" then
+          return
+        end
+
+        vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = lsp_group,
+          buffer = args.buf,
+          callback = function()
+            vim.lsp.buf.format({
+              async = false,
+              bufnr = args.buf,
+              filter = function(format_client)
+                return format_client.name == "iwes"
+              end,
+            })
+          end,
+        })
+      end,
+    })
+
     require("iwe").setup({
-      lsp = {
-        auto_format_on_save = true,
-        enable_inlay_hints = true,
-      },
       mappings = {
         leader = "<leader>i",
         enable_markdown_mappings = true,
